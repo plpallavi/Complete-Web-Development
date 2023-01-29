6800b06cf6d655d6beab0ef9ed7e168b667bd2c0
plpallavi <pallavi121202@gmail.com>
2023-01-29 20:40:10 +0530

    Web Dev

---
+++ WEB Development/webdev/JavaScript/Lecture1/index.js
@@ -0,0 +1,11 @@
+console.time("Time taken by your code to execute: ");
+console.log("This is Pallavi");
+console.log(90);
+console.log(44+6);
+console.log(true);
+console.log([1,2,3,4,5,6]);
+console.log({naam: "Pallavi" , marks: 100});
+console.table({naam: "Pallavi" , marks: 100});
+console.warn("This is warning");
+//console.clear();
+console.timeEnd("Time taken by your code to execute: ");
